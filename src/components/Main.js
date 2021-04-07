4cccb5e05f9286c7e155060527af44ca98415ddb
Jake Barber <jabot999@gmail.com>
2021-04-07 17:34:24 -0400

    visuals done for now

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,7 +21,7 @@ class Main extends Component {
                 const description = this.imageDescription.value
                 this.props.uploadImage(description)
               }} >
-                <input className="btn btn-primary" type='file' accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={this.props.captureFile} />
+                <input className="btn btn-secondary" type='file' accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={this.props.captureFile} />
                   <div className="form-group mr-sm-2">
                     <br></br>
                       <input
@@ -32,7 +32,7 @@ class Main extends Component {
                         placeholder="Image description..."
                         required />
                   </div>
-                <button type="submit" className="btn btn-primary btn-block btn-lg">Upload</button>
+                <button type="submit" className="btn btn-secondary btn-block btn-lg">Upload</button>
               </form>
 
               <p>&nbsp;</p>
@@ -55,7 +55,7 @@ class Main extends Component {
                     <ul id="imageList" className="list-group list-group-flush ">
                       <li className="list-group-item bg-secondary">
                         <p className="text-center"><img src={`https://ipfs.infura.io/ipfs/${image.hash}`} alt="#" style={{maxHeight: '50rem'}}/></p>
-                        <p className="text-dark">{image.description}</p>
+                        <p className="text-light border-info card-body">{image.description}</p>
                       </li>
                       <li key={key} className="list-group-item py-2">
                         <small className="float-left mt-1 text-muted ">
