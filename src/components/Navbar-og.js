ef354474f0dcf4c33ebc53b1232cbe115e0172e1
Jake Barber <jabot999@gmail.com>
2021-04-07 17:25:49 -0400

    added top button to navbar, still WIP

--- src/components/Navbar-og.js
+++ src/components/Navbar-og.js
@@ -6,25 +6,25 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">  
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        
+
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="https://www.jake-barber.com/#DSM"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp;Decentralized Social Media (DSM)
+          </a>
+
           
-            <ul className="navbar-nav m-auto">
-              <li className="nav-item">
-                <a
-                  className="navbar-brand col-sm-3 col-md-2 mr-0"
-                  href="https://www.jake-barber.com/#DSM"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
-                  &nbsp;Decentralized Social Media (DSM)
-                  </a>
-              </li>
-              
-              <li className="nav-item">
-                <a className="nav-link font-weight-bold" href="#header">BACK TO TOP</a>
-              </li>
-              <li className="nav-item">
+          
+
+
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
               <a
                 className="nav-link"
                 href={`https://rinkeby.etherscan.io/address/${this.props.account}`}
@@ -44,9 +44,8 @@ class Navbar extends Component {
                     : <span></span>
                 }
               </a>
-              </li>
-            </ul>
-          
+            </li>
+          </ul>
         
       </nav>
     );
