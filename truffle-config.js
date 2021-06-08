df16eb76715103a22b9c7694c7b27170080c5a78
Jake Barber <jabot999@gmail.com>
2021-06-08 16:19:20 -0400

    deployed to Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -25,7 +25,7 @@ module.exports = {
           `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
         )
       },
-      gas: 5000000,
+      gas: 10000000,
       gasPrice: 25000000000,
       network_id: 4 //rinkeby
     },
